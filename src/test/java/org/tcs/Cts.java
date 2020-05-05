d13dd6ba0f964aff8bd7a4c5f5c6832931b0d892
bharath1430 <blbharath44@gmail.com>
2020-05-05 14:25:18 +0530

    final conflict resolved

--- src/test/java/org/tcs/Cts.java
+++ src/test/java/org/tcs/Cts.java
@@ -1,12 +1,10 @@
 package org.tcs;
 
 public class Cts {
-public static void main(String[] args) {
-	System.out.println("Glass ");
-<<<<<<< HEAD
-	System.out.println("hellow ");
-=======
-	System.out.println("log");
->>>>>>> 05863eeb791fa4cdaccab2225afe93b5e41db504
-}
+	public static void main(String[] args) {
+		System.out.println("Glass ");
+		System.out.println("log");
+		System.out.println("hellow ");
+
+	}
 }
